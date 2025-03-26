d45dcb0adc8951a285e3297301733999d080960c
MiaoJiaWen <miao2627045617@163.com>
2025-03-26 17:32:57 +0800

    feat(monitor): 添加链路追踪和指标监控功能
    - 新增 Metrics 和 Tracer 的自动配置类
    - 添加 BizTrace 注解和切面，用于记录业务链路信息
    - 实现 TraceFilter过滤器，将 traceId 添加到响应头中
    - 增加 TracerFrameworkUtils 工具类，用于处理异常信
    - 添加 TracerProperties 配置类

---
+++ alone-framework/alone-spring-boot-starter-monitor/src/main/java/com/alone/coder/framework/tracer/config/TracerProperties.java
@@ -0,0 +1,15 @@
+package com.alone.coder.framework.tracer.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * BizTracer配置类
+ *
+ * @author 麻薯
+ */
+@ConfigurationProperties("alone.tracer")
+@Data
+public class TracerProperties {
+
+}
