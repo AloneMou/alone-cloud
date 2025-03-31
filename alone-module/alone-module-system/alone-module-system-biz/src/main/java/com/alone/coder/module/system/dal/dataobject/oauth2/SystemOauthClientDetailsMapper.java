a6c813d58b206acd1b82d566a4d8d485dd35f206
MiaoJiaWen <miao2627045617@163.com>
2025-03-31 10:32:34 +0800

    feat(mybatis): 添加 MyBatis 相关配置和工具类- 新增数据库配置类 AloneDataSourceAutoConfiguration - 新增 MyBatis 配置类 AloneMybatisAutoConfiguration - 新增基础实体对象 BaseDO - 新增扩展的 Mapper 接口 BaseMapperX - 新增数据源枚举 DataSourceEnum - 新增数据库类型枚举 DbTypeEnum - 新增通用参数填充处理器 DefaultDBFieldHandler - 新增 Druid 广告过滤器 DruidAdRemoveFilter - 新增字段加密处理器 EncryptTypeHandler - 新增 ID 类型环境处理器 IdTypeEnvironmentPostProcessor - 新增整数列表类型处理器 IntegerListTypeHandler - 新增 JDBC 工具类 JdbcUtils - 新增扩展的查询包装器 LambdaQueryWrapperX

---
+++ alone-module/alone-module-system/alone-module-system-biz/src/main/java/com/alone/coder/module/system/dal/dataobject/oauth2/SystemOauthClientDetailsMapper.java
@@ -0,0 +1,17 @@
+package com.alone.coder.module.system.dal.dataobject.oauth2;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author lengleng
+ * @since 2018-05-15
+ */
+@Mapper
+public interface SystemOauthClientDetailsMapper extends BaseMapperX<SysOauthClientDetails> {
+
+}
