e520c96c5e2a89351837e6fdeace81990c81bcdb
AloneMou <2627045617@qq.com>
2025-06-13 14:46:37 +0800

    feat(module): 新增 radius 模块并添加相关依赖
    - 新增 alone-module-radius 模块，包含 radius-api 子模块
    - 在系统模块中添加 radius-api依赖
    - 新增 SeeProApi 接口并实现相关请求方法
    - 添加日志配置文件 logback-spring.xml
    - 更新 application-local.yaml 配置

--- alone-module/alone-module-system/alone-module-system-biz/src/main/java/com/alone/coder/module/system/controller/route/SystemRouteController.java
+++ alone-module/alone-module-system/alone-module-system-biz/src/main/java/com/alone/coder/module/system/controller/route/SystemRouteController.java
@@ -1,7 +1,10 @@
 package com.alone.coder.module.system.controller.route;
 
+import com.alone.coder.module.seepro.api.SeeProApi;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
@@ -9,4 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SystemRouteController {
 
+    @Resource
+    private SeeProApi seeProApi;
+
+    @GetMapping(value = "/test")
+    public void request() {
+        System.out.println("请求路由");
+        String body = seeProApi.getWebSocketUrl("", "1", "{}");
+        System.out.println(body);
+        System.out.println("请求结束");
+    }
 }
