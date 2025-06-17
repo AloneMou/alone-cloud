91b426e4a8a8d4e281f966b4a950578443f5737d
AloneMou <2627045617@qq.com>
2025-06-17 11:56:51 +0800

    build(alone-module): 更新依赖并移除未使用的代码
    - 在 pom.xml 中添加了 redis 缓存和灰度路由的依赖
    - 移除了 SystemRouteController 中未使用的 SeeProApi 相关代码

--- alone-module/alone-module-system/alone-module-system-biz/src/main/java/com/alone/coder/module/system/controller/route/SystemRouteController.java
+++ alone-module/alone-module-system/alone-module-system-biz/src/main/java/com/alone/coder/module/system/controller/route/SystemRouteController.java
@@ -1,6 +1,5 @@
 package com.alone.coder.module.system.controller.route;
 
-import com.alone.coder.module.seepro.api.SeeProApi;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.springframework.validation.annotation.Validated;
@@ -12,14 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SystemRouteController {
 
-    @Resource
-    private SeeProApi seeProApi;
 
-    @GetMapping(value = "/test")
-    public void request() {
-        System.out.println("请求路由");
-        String body = seeProApi.getWebSocketUrl("", "1", "{}");
-        System.out.println(body);
-        System.out.println("请求结束");
-    }
+
 }
